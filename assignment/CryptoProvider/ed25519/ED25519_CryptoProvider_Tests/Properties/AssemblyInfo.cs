f16b8f74586eb7b771758083385cd9d8c3aeb8f7
Paul Haines <40448091@live.napier.ac.uk>
2020-03-06 00:34:02 +0000

    fixed test issue with ed25519

---
+++ assignment/CryptoProvider/ed25519/ED25519_CryptoProvider_Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ED25519_CryptoProvider_Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ED25519_CryptoProvider_Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f66b67a9-861c-40d3-85f5-efbed297c9a2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
